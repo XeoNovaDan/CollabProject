530f27c92bb7606cc4b30f3ef551f0b534397fc9
XeoNovaDan <toghill7252010@hotmail.co.uk>
2018-08-29 23:05:38 +0100

    buggy bionic grenades

---
+++ Source/CollabProject/Codename Project RIM/Classes/DamageWorkers/DamageWorker_BionicGrenade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace Codename_Project_RIM
+{
+    class DamageWorker_BionicGrenade : DamageWorker_AddInjury
+    {
+
+        private const float BaseChanceToAddHediff = 0.5f;
+
+        protected override void ExplosionDamageThing(Explosion explosion, Thing t, List<Thing> damagedThings, IntVec3 cell)
+        {
+            base.ExplosionDamageThing(explosion, t, damagedThings, cell);
+            List<HediffDef> validHediffs = DefDatabase<HediffDef>.AllDefs.Where(h => h.hediffClass == typeof(Hediff_AddedPart) || h.hediffClass == typeof(Hediff_Implant)).ToList();
+            float hediffChance = BaseChanceToAddHediff / validHediffs.Count;
+            foreach (Thing thing in damagedThings)
+                if (thing is Pawn pawn && pawn.health is Pawn_HealthTracker health)
+                    foreach (HediffDef hediff in validHediffs)
+                        if (Rand.Chance(hediffChance))
+                            health.AddHediff(hediff, pawn.RaceProps.body.AllParts.RandomElement());
+        }
+
+    }
+}
